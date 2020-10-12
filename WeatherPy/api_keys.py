75b4bd787c7dfcf18045a591a0d068b4751f4c27
dkuchan415 <danielkuchan@yahoo.com>
2020-10-12 18:11:18 -0500

    Added for loop for city information and KeyError exception

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5ec832b67261dfab5c709baaaf1fdfe7"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBvsEHAyd47Zh5bltWrwD9WddARgjVOMnE"
